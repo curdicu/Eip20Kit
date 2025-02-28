3da2c0166fb59bf0dd880089a092d974182c82fb
curdicu <curdicu@gmail.com>
2025-02-28 21:43:34 +0800

    *

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/curdicu/EvmKit.git", .branch("main")),
-        .package(url: "https://github.com/curdicu/HsCryptoKit.git", .branch("main")),
-        .package(url: "https://github.com/curdicu/HsExtensions.git", .branch("main")),
+        .package(url: "https://github.com/horizontalsystems/EvmKit.Swift.git", .upToNextMajor(from: "2.0.5")),
+        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
     ],
     targets: [
         .target(
@@ -25,9 +25,9 @@ let package = Package(
             dependencies: [
                 "BigInt",
                 .product(name: "GRDB", package: "GRDB.swift"),
-                .product(name: "EvmKit", package: "EvmKit"),    
-                .product(name: "HsCryptoKit", package: "HsCryptoKit"),
-                .product(name: "HsExtensions", package: "HsExtensions"),
+                .product(name: "EvmKit", package: "EvmKit.Swift"),
+                .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
+                .product(name: "HsExtensions", package: "HsExtensions.Swift"),
             ]
         ),
     ]
